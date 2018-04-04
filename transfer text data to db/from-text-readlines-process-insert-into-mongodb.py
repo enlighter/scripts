88a8481fa48d700de022aee4c1366a27e7c18d0e
Sushovan Mandal <sushovan@alumnux.com>
2018-04-04 13:56:43 +0530

    state save

--- transfer text data to db/from-text-readlines-process-insert-into-mongodb.py
+++ transfer text data to db/from-text-readlines-process-insert-into-mongodb.py
@@ -3,7 +3,11 @@ import json
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client.psva
-print(db['drugs'])
+
+data_list = []
+
+with open('drug_mongo_json.txt') as drugs_file:
+	data_list = drugs_file.readlines()
 inserted_id = db['drugs'].insert_one(json.loads('{ drug_id : 1, name : "Lepirudin" }')).inserted_id
 
 print(inserted_id)
